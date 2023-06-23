7342fdfa3bdb65809006872aeed28a12de0c67ec
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-06-22 22:15:10 -0400

    creating a blog

---
+++ client/src/Posts.js
@@ -0,0 +1,32 @@
+export default function Posts() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://assets1.cbsnewsstatic.com/hub/i/r/2023/03/08/60d7b07a-9f72-45e1-b28e-02c904b28aec/thumbnail/620x413/de2178ca911c58be6b964598566cbabe/gettyimages-1216705608.jpg?v=74b410729cdff54299e4158e8919d920" />
+            </div>
+            <div className="texts">
+                <h2>Bear Safety</h2>
+                <p className="info">
+                    <a className="author">Zukkii</a>
+                    <time>2023-6-22 9:55</time>
+                </p>
+                <p className="summary">
+                    Mauris id urna eget velit bibendum sodales. Quisque quis
+                    blandit ipsum, in ultricies diam. Nullam eget lacinia
+                    turpis, eu molestie diam. Nulla facilisis posuere erat, sed
+                    facilisis elit. Donec mattis, sem ac sodales vestibulum,
+                    ipsum ante gravida arcu, ac eleifend erat mauris ut massa.
+                    Suspendisse ullamcorper, mi quis laoreet bibendum, neque
+                    metus dapibus nunc, id fermentum mi metus at neque.
+                    Curabitur in sodales orci, eu vulputate dui. Proin lectus
+                    dui, volutpat porttitor arcu ut, tempor bibendum ligula.
+                    Phasellus pharetra nisi ac bibendum ultrices. Nunc tempus
+                    sed dolor in vehicula. Morbi quis finibus arcu. Nam vel
+                    luctus lorem, sed luctus neque. Proin dignissim ultricies
+                    purus molestie tempus. Vivamus nec pellentesque justo. Duis
+                    nec nulla dapibus, condimentum arcu quis, fermentum eros.
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Posts from "../Posts";
+
+export default function IndexPage() {
+    return (
+        <div>
+            <Posts />
+            <Posts />
+            <Posts />
+        </div>
+    );
+}
